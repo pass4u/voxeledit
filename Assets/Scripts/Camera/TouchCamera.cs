afa84a7921dbad652fa98b4a4b580757ce9ab8a4
brian <brian@bigmanent.com>
2016-02-25 22:06:44 +0900

    초기셋팅

---
+++ Assets/Scripts/Camera/TouchCamera.cs
@@ -0,0 +1,89 @@
+﻿// Just add this script to your camera. It doesn't need any configuration.
+
+using UnityEngine;
+
+public class TouchCamera : MonoBehaviour {
+
+	private GameObject camera;
+
+	Vector2?[] oldTouchPositions = {
+		null,
+		null
+	};
+	Vector2 oldTouchVector;
+	float oldTouchDistance;
+
+	void Start(){
+		camera = gameObject.transform.FindChild ("Main Camera").gameObject;
+	}
+
+	void Update() {
+		if (Input.touchCount == 0) {
+			oldTouchPositions[0] = null;
+			oldTouchPositions[1] = null;
+		}
+		else if (Input.touchCount == 1) {
+            Ray ray = camera.GetComponent<Camera>().ScreenPointToRay(Input.GetTouch(0).position);
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                print("I'm looking at " + hit.transform.name);
+            else
+                print("I'm looking at nothing!");
+            /*
+			// single touch
+
+			// single touch start
+			if (oldTouchPositions[0] == null || oldTouchPositions[1] != null) {
+				oldTouchPositions[0] = Input.GetTouch(0).position;
+				oldTouchPositions[1] = null;
+			}
+
+			// single touch drag
+			else {
+				Vector2 newTouchPosition = Input.GetTouch(0).position;
+				Vector2 v1 = (Vector2)oldTouchPositions[0] - newTouchPosition;
+				Vector3 vv = (Vector3)((new Vector2(-v1.y,v1.x)) * camera.GetComponent<Camera>().orthographicSize / camera.GetComponent<Camera>().pixelHeight * 200f);
+				Debug.Log (vv);
+				transform.Rotate(vv);
+//				transform.rotation += transform.r
+				oldTouchPositions[0] = newTouchPosition;
+			}
+             */
+		}
+		else {
+			// multi touch
+			if (oldTouchPositions[1] == null) {
+				oldTouchPositions[0] = Input.GetTouch(0).position;
+				oldTouchPositions[1] = Input.GetTouch(1).position;
+				oldTouchVector = (Vector2)(oldTouchPositions[0] - oldTouchPositions[1]);
+				oldTouchDistance = oldTouchVector.magnitude;
+			}
+			else {
+				Vector2 screen = new Vector2(camera.GetComponent<Camera>().pixelWidth, camera.GetComponent<Camera>().pixelHeight);
+				
+				Vector2[] newTouchPositions = {
+					Input.GetTouch(0).position,
+					Input.GetTouch(1).position
+				};
+				Vector2 newTouchVector = newTouchPositions[0] - newTouchPositions[1];
+				float newTouchDistance = newTouchVector.magnitude;
+
+				transform.position += transform.TransformDirection((Vector3)((oldTouchPositions[0] + oldTouchPositions[1] - screen) * camera.GetComponent<Camera>().orthographicSize / screen.y));
+				transform.localRotation *= Quaternion.Euler(new Vector3(0, 0, Mathf.Asin(Mathf.Clamp((oldTouchVector.y * newTouchVector.x - oldTouchVector.x * newTouchVector.y) / oldTouchDistance / newTouchDistance, -1f, 1f)) / 0.0174532924f));
+				camera.GetComponent<Camera>().orthographicSize *= oldTouchDistance / newTouchDistance;
+				transform.position -= transform.TransformDirection((newTouchPositions[0] + newTouchPositions[1] - screen) * camera.GetComponent<Camera>().orthographicSize / screen.y);
+
+				oldTouchPositions[0] = newTouchPositions[0];
+				oldTouchPositions[1] = newTouchPositions[1];
+				oldTouchVector = newTouchVector;
+				oldTouchDistance = newTouchDistance;
+			}
+		}
+
+
+       
+
+
+	}
+}
---
+++ Assets/Scripts/ContainerControl.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContainerControl : MonoBehaviour {
+	public GameObject Container;
+
+	public GameObject camera;
+
+	public int i_bottom_guide_row, i_botton_guide_cell;
+	public GameObject pref_guide_item;
+	private Vector3 q;
+	private GameObject container_anchor, bottom_guide;
+	// Use this for initialization
+	void Start () {
+        //ViewCenter ();
+        //ViewAllObject ();
+
+		container_anchor = Container.transform.FindChild ("Anchor").gameObject;
+		GenerateGuide ();
+	}
+
+	/// <summary>
+	/// generate guide
+	/// </summary>
+	public void GenerateGuide(){
+
+		if (container_anchor.transform.FindChild ("bottom_guide") == null) {
+			// generate !!
+			bottom_guide = new GameObject();
+			bottom_guide.name = "bottom_guide";
+			bottom_guide.transform.parent = container_anchor.transform;
+			bottom_guide.transform.localPosition = Vector3.down*0.5f;
+			bottom_guide.transform.localRotation = Quaternion.Euler (Vector3.zero);
+
+		}
+
+		int row_count = 0;
+		GameObject pref;
+		for (int i = 0; i < i_botton_guide_cell; ++i) {
+			for (int j = 0; j < i_bottom_guide_row; ++j) {
+				pref = Instantiate (pref_guide_item);
+				pref.transform.parent = bottom_guide.transform;
+				pref.transform.localPosition = new Vector3(-1*row_count,0,(-1*j));
+
+			}
+			row_count += 1;
+		}
+	}
+
+	public void horizonSCBHandler(float value){
+		Debug.Log (value);
+//		value -> 0 = 0->180
+//		value -> 1 = 360->180
+
+		Vector3 q = Container.transform.rotation.eulerAngles;
+		Vector3 v = new Vector3 (q.x, Mathf.Lerp (180, -180, value), q.z);
+        Container.transform.rotation = Quaternion.Euler(v);
+	}
+
+	public void verticalSCBHandler(float value){
+		Debug.Log (value);		
+		Quaternion q = Container.transform.rotation;
+        //Vector3 v = new Vector3 (Mathf.Lerp (180, -180, value),q.y, q.z);
+        Container.transform.rotation = Quaternion.Euler(new Vector3(Mathf.Lerp(180, -180, value), q.y, q.z));
+
+	}
+
+	public void ViewCenter(){
+		camera.transform.position=Vector3.zero;
+		setCenterForObject ();
+	}
+
+
+	// set center for all child gameobject
+	void setCenterForObject()
+	{
+//		Debug.Log (Container.transform.FindChild ("Anchor").localPosition);
+
+
+		Vector3 centroid = Vector3.zero;
+		if ( Container.transform.FindChild("Anchor").childCount > 0 )
+		{
+			Transform allChildren = Container.transform.FindChild("Anchor").GetComponentInChildren<Transform>();
+			foreach (Transform child in allChildren)
+			{
+				centroid += child.localPosition;
+			}
+			centroid /= -allChildren.childCount;
+		}
+
+
+//		Debug.Log ("centroid :: " + centroid);
+
+		if (Container.transform.FindChild ("Anchor").localPosition != centroid) {
+			Container.transform.FindChild("Anchor").localPosition = centroid;
+		}
+
+	}
+
+	public void ViewAllObject(){
+		Vector3 centroid = Vector3.zero;
+		Transform allChildren = Container.transform.FindChild("Anchor").GetComponentInChildren<Transform>();
+		foreach (Transform child in allChildren)
+		{
+			centroid += child.localScale;
+		}
+
+		float max = Mathf.Max (centroid.x, centroid.y, centroid.z);
+		if (max <= 1) {
+			max = 1;
+		} else {
+			max /=2;
+		}
+		Debug.Log ("max :: " + max);
+		camera.transform.FindChild ("Main Camera").GetComponent<Camera> ().orthographicSize = max;
+
+	}
+
+}
